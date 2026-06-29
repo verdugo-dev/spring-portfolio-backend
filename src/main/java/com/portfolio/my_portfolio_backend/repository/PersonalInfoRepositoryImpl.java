822dcd5980b501300140440b7093848701036527
Carlos Verdugo <carlos.versa99@gmail.com>
2026-06-28 18:31:14 -0600

    Adding keyholder id on save Personal Info

--- src/main/java/com/portfolio/my_portfolio_backend/repository/PersonalInfoRepositoryImpl.java
+++ src/main/java/com/portfolio/my_portfolio_backend/repository/PersonalInfoRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.portfolio.my_portfolio_backend.repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.portfolio.my_portfolio_backend.model.PersonalInfo;
@@ -35,8 +39,65 @@ public class PersonalInfoRepositoryImpl implements IPersonalInfoRepository {
     @Override
     public PersonalInfo save(PersonalInfo personalInfo) {
         if (personalInfo.getId() == null) {
-            String sql = "";
+            String sql = "INSERT INTO personal_info (first_name, last_name, title, profile_description, profile_image_url, years_of_experience, email, phone, linkedin_url, github_url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+
+            jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(sql, new String []{"id"});
+
+                ps.setString(1, personalInfo.getFirstName());
+                ps.setString(2, personalInfo.getLastName());
+                ps.setString(3, personalInfo.getTitle());
+                ps.setString(4, personalInfo.getProfileDescription());
+                ps.setString(1, personalInfo.getProfileImageUrl());
+
+                if (personalInfo.getYearsOfExperience() != null) {
+                    ps.setInt(6, personalInfo.getYearsOfExperience());
+                
+                } else {
+                    ps.setNull(6, java.sql.Types.INTEGER);
+                }
+
+                ps.setString(7, personalInfo.getEmail());
+                ps.setString(8, personalInfo.getPhone());
+                ps.setString(9, personalInfo.getLinkedinUrl());
+                ps.setString(1, personalInfo.getGithubUrl());
+
+                return ps;
+
+            }, keyHolder);
+
+            personalInfo.setId(Objects.requireNonNull(keyHolder.getKey()).longValue());
+
+            // jdbcTemplate.update(sql, 
+            //     personalInfo.getFirstName(),
+            //     personalInfo.getLastName(),
+            //     personalInfo.getTitle(),
+            //     personalInfo.getProfileDescription(),
+            //     personalInfo.getProfileImageUrl(),
+            //     personalInfo.getYearsOfExperience(),
+            //     personalInfo.getEmail(),
+            //     personalInfo.getPhone(),
+            //     personalInfo.getLinkedinUrl(),
+            //     personalInfo.getGithubUrl()
+            // );
+
         } else {
+            String sql = "UPDATE personal_info SET first_name=?, last_name=?, title=?, profile_description=?, profile_image_url=?, years_of_experience=?, email=?, phone=?, linkedin_url=?, github_url=? WHERE id=?";
+
+            jdbcTemplate.update(sql,
+                personalInfo.getFirstName(),
+                personalInfo.getLastName(),
+                personalInfo.getTitle(),
+                personalInfo.getProfileDescription(),
+                personalInfo.getProfileImageUrl(),
+                personalInfo.getYearsOfExperience(),
+                personalInfo.getEmail(),
+                personalInfo.getPhone(),
+                personalInfo.getLinkedinUrl(),
+                personalInfo.getGithubUrl()
+            );
 
         }
 
